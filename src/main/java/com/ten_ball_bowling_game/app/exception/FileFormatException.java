d86308f9aca7cab80da62bdb9bc34b2373003913
Deon Stewart <deon.c.stewart@gmail.com>
2021-05-14 11:51:17 -0500

    update frame sum

---
+++ src/main/java/com/ten_ball_bowling_game/app/exception/FileFormatException.java
@@ -0,0 +1,8 @@
+package com.ten_ball_bowling_game.app.exception;
+
+public class FileFormatException extends Exception {
+
+    public FileFormatException() {
+        super();
+    }
+}
---
+++ src/main/java/com/ten_ball_bowling_game/app/scoreboard/FrameSum.java
@@ -0,0 +1,28 @@
+package com.ten_ball_bowling_game.app.scoreboard;
+
+import com.ten_ball_bowling_game.app.interfaces.IFrame;
+
+public class FrameSum {
+    private int frameCounter;
+    private IFrame frame;
+    private int total;
+
+    public FrameSum(int counter, IFrame frame, int total) {
+        this.frameCounter = counter;
+        this.frame = frame;
+        this.total = total;
+    }
+
+    public int getTotal() {
+        return this.total;
+    }
+
+    public IFrame getFrame() {
+        return this.frame;
+    }
+
+    public int getFrameCounter() {
+        return this.frameCounter;
+    }
+
+}
