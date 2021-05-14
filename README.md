1295d080c62614bfd3c492773e24949c9d8cde60
Deon Stewart <deon.c.stewart@gmail.com>
2021-05-14 11:44:11 -0500

    update to the Game entity

--- README.md
+++
@@ -1,1 +0,0 @@
-# 10pinbowling_resultsreader
---
+++ src/test/java/com/ten_ball_bowling_game/app/game/GameTest.java
@@ -0,0 +1,143 @@
+package com.ten_ball_bowling_game.app.game;
+
+import com.ten_ball_bowling_game.app.exception.InvalidPlayInputException;
+import com.ten_ball_bowling_game.app.exception.InvalidRollAttemptException;
+import com.ten_ball_bowling_game.app.game.utils.GameFactory;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class GameTest {
+    @Mock
+    private GameFactory gameFactory;
+
+    @Test
+    public void testGameStartAtZeroAtInitial() {
+        Game game = new Game(null);
+        Assert.assertEquals("Game starts at zero", 0, game.getTotalScore());
+    }
+
+    @Test
+    public void testFirstRollWithSuccess() throws InvocationTargetException, IllegalAccessException {
+        GameFactory gameFactory = new GameFactory();
+        Game game = new Game(gameFactory);
+
+        game.roll("4");
+
+        Assert.assertEquals(4, game.getTotalScore());
+    }
+
+    @Test(expected = InvalidPlayInputException.class)
+    public void testRollWithInvalidEntryActual() throws IllegalAccessException, InvocationTargetException {
+        GameFactory innerGameFactory = new GameFactory();
+        Game game = new Game(innerGameFactory);
+        game.roll("R");
+    }
+
+    @Test
+    public void testCreationOfFramesBasedOnIfCurrentFrameIsDone() throws  InvocationTargetException, IllegalAccessException {
+        GameFactory innerGameFactory = new GameFactory();
+        Game game = new Game(innerGameFactory);
+
+        game.roll("3");
+        game.roll("9");
+        game.roll("F");
+
+        Assert.assertEquals(2, game.getFrameSum().size());
+    }
+
+    @Test(expected = InvalidPlayInputException.class)
+    public void testInvalidPlayIfTotalRollNotEqualToTen() throws InvocationTargetException, IllegalAccessException {
+        GameFactory innerGameFactory = new GameFactory();
+        Game game = new Game(innerGameFactory);
+
+        game.roll("3");
+        game.roll("9");
+        game.roll("F");
+    }
+
+    @Test
+    public void testCalculateScoreWithSpare() throws InvocationTargetException, IllegalAccessException {
+        GameFactory factory = new GameFactory();
+        Game game =  new Game(factory);
+
+        game.roll("10");
+        game.roll("7");
+        game.roll("3");
+        game.roll("F");
+        game.roll("4");
+
+        Assert.assertEquals(34, game.getTotalScore());
+    }
+
+    @Test
+    public void testCalculateFullGame() throws InvocationTargetException, IllegalAccessException {
+        GameFactory factory = new GameFactory();
+        Game game =  new Game(factory);
+
+        game.roll("10");
+        game.roll("7");
+        game.roll("3");
+        game.roll("9");
+        game.roll("0");
+        game.roll("10");
+        game.roll("0");
+        game.roll("8");
+        game.roll("8");
+        game.roll("2");
+        game.roll("F");
+        game.roll("6");
+        game.roll("10");
+        game.roll("10");
+        game.roll("10");
+        game.roll("8");
+        game.roll("1");
+
+        Assert.assertEquals(167, game.getTotalScore());
+
+    }
+
+    @Test
+    public void testCalculateFullScoreWithStrikes() throws  InvocationTargetException, IllegalAccessException {
+        GameFactory factory = new GameFactory();
+        Game game =  new Game(factory);
+
+        game.roll("10");
+        game.roll("10");
+        game.roll("10");
+        game.roll("10");
+        game.roll("10");
+        game.roll("10");
+        game.roll("10");
+        game.roll("10");
+        game.roll("10");
+        game.roll("10");
+        game.roll("10");
+        game.roll("10");
+
+        Assert.assertEquals(300, game.getTotalScore());
+        Assert.assertTrue(game.isPerfectGame());
+    }
+
+    @Test
+    public void testCalculateFullScoreWithGutter() throws  InvocationTargetException, IllegalAccessException {
+        GameFactory factory = new GameFactory();
+        Game game =  new Game(factory);
+
+        game.roll("0");
+        game.roll("0");
+        game.roll("0");
+        game.roll("0");
+        game.roll("0");
+        game.roll("0");
+        game.roll("0");
+        game.roll("0");
+        game.roll("0");
+        game.roll("0");
+
+        Assert.assertEquals(0, game.getTotalScore());
+        Assert.assertTrue(game.isGutterGame());
+    }
+}
